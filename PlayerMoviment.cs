27998d33a4e1f8e89c55db56849c7be49afe51bc
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-02-06 21:01:22 -0300

    [ADDED] Added script template player moviment and look to mouse pointer

---
+++ PlayerMoviment.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovimentLookingMouse : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Rigidbody rb;
+
+    Vector3 direction;
+
+    public GameObject gameOverText;
+
+    //public bool isAlive = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Xaxis = Input.GetAxis("Horizontal");
+        float Zaxis = Input.GetAxis("Vertical");
+
+        direction = new Vector3(Xaxis, 0, Zaxis);
+
+        
+
+        //transform.Translate(direction * speed * Time.deltaTime);
+
+        if (direction != Vector3.zero)
+        {
+            //anim.SetBool("Running", true);
+        }
+        else
+        {
+            //anim.SetBool("Running", false);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (direction * speed * Time.deltaTime)); // Move player from rigibody position
+
+        Ray radius = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(radius.origin, radius.direction * 100, Color.red);
+
+        RaycastHit impact; // check collision
+
+        if(Physics.Raycast(radius, out impact, 100, floorMask))
+        {
+            Vector3 playerAimPosition = impact.point - transform.position;
+
+            playerAimPosition.y = transform.position.y;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerAimPosition);
+
+            rb.MoveRotation(newRotation);
+        }
+    }
+}
