5183960221ac04bdd218ff17fdaea85778ba8ab4
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-02-13 22:10:03 -0300

    Update PlayerMoviment

--- PlayerMoviment.cs
+++ PlayerMoviment.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Player move using rigidbody from Unity using AWSD or arrows. Use in 3D games.
+// Player move using rigidbody from Unity using AWSD or arrows, 
 
 public class PlayerMoviment : MonoBehaviour
 {
